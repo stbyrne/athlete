d322f85cee9326df61ea218325fd1ea092debd21
stbyrne <stuartbyrne@Stuarts-MacBook-Pro.local>
2016-08-22 23:05:15 +0100

    commit_master_6

--- app/pages/item-edit-modal/item-edit-modal.ts
+++ app/pages/item-edit-modal/item-edit-modal.ts
@@ -1,36 +1,25 @@
 import { Component } from '@angular/core';
 import {NavController, NavParams, ModalController, ViewController} from 'ionic-angular';
-import {FORM_DIRECTIVES, FormBuilder,  ControlGroup, Validators, AbstractControl} from '@angular/common';
-import {CustomValidators} from '../validators/CustomValidators.ts';
-/*import {ProfileDetailPage} from '../profile-detail/profile-detail.ts';*/
-/*import { ItemEditModal } from './item-edit-modal';*/
-
 
 @Component({
-  templateUrl: 'build/pages/item-edit-modal/item-edit-modal.html',
-  directives: [FORM_DIRECTIVES]
+  templateUrl: 'build/pages/item-edit-modal/item-edit-modal.html'
 })
 
 export class ItemEditModalPage {
   selectedEditable: any;
-  authForm: ControlGroup;
-  item: AbstractControl;
+  /*item: any;*/
     
-  constructor (public viewCtrl: ViewController, navParams: NavParams, private fb: FormBuilder){
+  constructor (public viewCtrl: ViewController, navParams: NavParams){
       
     this.selectedEditable = navParams.get('editable');
       
     console.log(this.selectedEditable);
       
-    this.authForm = fb.group({  
-        'item': ['', Validators.compose([Validators.required, Validators.minLength(3), CustomValidators.checkFirstCharacterValidator])]
-    });
-
-    this.item = this.authForm.controls['item'];
       
+  }
     
 
-  }
+  
   update() {
     this.selectedEditable;
     this.viewCtrl.dismiss();
