0ec96e60de6f3e4967b7454c2185970b0aeaf384
stbyrne <stuart.byrne@hmhco.com>
2016-09-05 12:28:54 +0100

    commit_work_9

--- app/pages/item-edit-modal/item-edit-modal.ts
+++ app/pages/item-edit-modal/item-edit-modal.ts
@@ -1,41 +1,64 @@
 import { Component } from '@angular/core';
 import {NavController, NavParams, ModalController, ViewController} from 'ionic-angular';
 import {DatePicker} from 'ionic-native';
+import {jsonfile} from 'jsonfile';
+/*import {FormBuilder, Validators} from '@angular/forms';*/
 
 @Component({
   templateUrl: 'build/pages/item-edit-modal/item-edit-modal.html'
 })
 
+
 export class ItemEditModalPage {
   selectedEditable: any;
   selectedEditableCategory: any;
   selectedEditableUnit: any;
-  /*item: any;*/
+  updatedDate : any;
+  updatedEditable : string;
+  myForm: any;
     
-  constructor (public viewCtrl: ViewController, navParams: NavParams){
+/*static get parameters() {
+  return [[FormBuilder]];
+}*/
+    
+constructor (public viewCtrl: ViewController, navParams: NavParams){
       
     this.selectedEditable = navParams.get('editable');
     this.selectedEditableCategory = navParams.get('category');
     this.selectedEditableUnit = navParams.get('unit');
-      
+    
     console.log(this.selectedEditable);
     console.log(this.selectedEditableCategory);
     console.log(this.selectedEditableUnit);
       
-    /*myDate: String = new Date().toISOString();
-      */
+    /*this.myForm = formBuilder.group({
+      editableItem: ['initial value goes here']
+    });*/
       
   }
     
-
-  
-  update() {
-    this.selectedEditable;
+    
+  updateSelectedEditable() {
+      
+    console.log('From Item Edit Modal: ' + this.updatedEditable);
+    console.log('New Date: ' + this.updatedDate);
+      
+    if(this.updatedEditable){
+        this.selectedEditable = this.updatedEditable;
+    }
+    if(this.updatedDate){
+        this.selectedEditable = this.updatedDate;
+    } 
+     
+      
+    console.log('New Editable: ' + this.selectedEditable);
+    
+          
     this.viewCtrl.dismiss();
     
   }
     
-  dismiss() {
+  dismissSelectedEditable() {
     this.viewCtrl.dismiss();
   }
 
